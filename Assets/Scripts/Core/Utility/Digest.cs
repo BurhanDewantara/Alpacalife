81907465ee00981f8485ac326e8a2a13e5fdde2e
Burhan Dewantara <burhan.dewantara@artoncode.net>
2016-01-11 18:15:36 +0700

    fixing new InfCurrency

--- Assets/Scripts/Core/Utility/Digest.cs
+++ Assets/Scripts/Core/Utility/Digest.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Security.Cryptography;
 using System.Text;
 using System;
--- Assets/testing.cs
+++ Assets/testing.cs
@@ -1,25 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using ScottGarland;
 
 public class testing : MonoBehaviour {
+
+	BigInteger aa;
+	BigInteger bb;
+	InfCurrency x;
+	InfCurrency a;
+	InfCurrency b;
 
 	// Use this for initialization
 	void Start () {
 	
 //		Currency x = new Currency (100,CurrencyUnit.V);
-//		Currency x = new Currency (12345667890987654321);
-		Currency x = new Currency ("123");
-		Currency y = new Currency ("123");
+		a = new InfCurrency ("99999999999999999999999999999999999999999999999999999999999999999999999999999999999");
+		b = new InfCurrency ("1");
+//		InfCurrency y = new InfCurrency (1);
+//		x = new InfCurrency ("10");
 
-		x = x - y; 
+		Debug.Log(b-a);
 
+//		x = x + x; 
+//		Debug.Log(x);
+//
 //		Debug.Log("x < y " + (x < y));
 //		Debug.Log("x > y " + (x > y));
 //
 //		Debug.Log("x <= y " + (x <= y));
 //		Debug.Log("x >= y " + (x >= y));
 
+		aa = new BigInteger("11");
+		bb = new BigInteger("22");
+		BigInteger cc = aa - bb;
+		Debug.Log(cc);
+		cc+=bb;
+		Debug.Log(cc);
+
+
+
+
 	}
+
+
+	void OnGUI()
+	{
+//		a-=b;
+//		GUI.Label(new Rect(0,20,Screen.width,Screen.height),a.DefaultString());
+
+//		for (int i=0; i<100; i++) {
+//			aa += aa;
+//		}
+//		GUI.Label(new Rect(0,20,Screen.width,Screen.height),aa.ToString());
+
+//		for (int i=0; i<100; i++) {
+//		x = x + x ;
+//		}
+//		GUI.Label(new Rect(0,20,Screen.width,Screen.height),x.DefaultString());
+	}
+
+		
 	
 
 }
