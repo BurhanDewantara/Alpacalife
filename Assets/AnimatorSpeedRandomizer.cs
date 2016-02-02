e1f15cafdef2b9ac0bb4afb331af34a16abe16f2
Burhan Dewantara <burhan.dewantara@artoncode.net>
2016-02-02 18:22:32 +0700

    remove 3 alpaca. adding tag adding animator

---
+++ Assets/AnimatorSpeedRandomizer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorSpeedRandomizer : MonoBehaviour {
+
+	public float minAnimationSpeed =0.8f;
+	public float maxAnimationSpeed =1.2f;
+
+	void Start () {
+		this.GetComponent<Animator>().speed = Random.Range(minAnimationSpeed, maxAnimationSpeed);
+	}
+}
--- Assets/LivestockController.cs
+++ Assets/LivestockController.cs
@@ -6,11 +6,75 @@ using Artoncode.Core.Utility;
 
 public class LivestockController : CharacterCanvasController {
 
+	LivestockColorHandler colorHandler;
+
+	public bool isReady = false;
+	public bool isOrdered = false;
+
+
+	void Awake()
+	{
+		
+	}
+
+
+
+	void MoveLeft()
+	{
+		velocity = Vector2.left * moveSpeed;
+	}
+
+	void MoveRight()
+	{
+		velocity = Vector2.right * moveSpeed;
+	}
+
+	void MoveDown()
+	{
+		velocity = Vector2.down * moveSpeed;
+
+	}
+
+	void MoveUp()
+	{
+		velocity = Vector2.up * moveSpeed ;
+
+	}
+
+
+	void InputUpdate()
+	{
+		if (isInteractable) {
+			////			velocity = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")) * moveSpeed;
+			if (Input.GetAxisRaw ("Horizontal") == 1) {
+				MoveRight ();
+			}
+			if (Input.GetAxisRaw ("Horizontal") == -1) {
+				MoveLeft ();
+			}
+			if (Input.GetAxisRaw ("Vertical") == -1) {
+				MoveDown ();
+			}
+			if (Input.GetAxisRaw ("Vertical") == 1) {
+				MoveUp ();
+			}
+			if (Input.GetButton("Jump")) {
+				velocity = Vector2.zero;
+			}
+		}
+	}
+
+	void Update()
+	{
+		InputUpdate();
+		MovementUpdate();
+	}
+
+
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.CompareTag("Livestock"))
+		if (col.CompareTag("Fence"))
 		{
-			
 			Jump();				
 		}
 
